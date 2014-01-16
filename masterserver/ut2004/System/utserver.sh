8eb372b4db6558cf37278d8d9dc106ec859f5e51
Donzi <Donzi@Despe.de>
2014-01-16 20:01:30 +0100

    UT Wrapper Script + adjustment of XML
    Wrapper, because Easy-Wi overwrites Server Logs and Crash Logs are not
    produced

---
+++ masterserver/ut2004/System/utserver.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+###########################################################
+#                                                         #
+#       =========================================         #
+#       |       DNW Despe Networks              |         #
+#       =========================================         #
+#       |>>>>> UT-Server-Wrapper for Easy-Wi >>>|         #
+#       |>>>>> http://www.despe.de >>>>>>>>>>>>>|         #
+#       |>>>>> DO NOT EDIT, only if u know >>>>>|         #
+#       |>>>>> what are you doing! >>>>>>>>>>>>>|         #
+#       =========================================         #
+#                                                         #
+###########################################################
+
+#	Timestamp for the Crash or Restart Logs
+STAMP=`date +%d-%m-%Y_%H%M%S`
+
+#	Remove old logs after x Days
+DAYS="2";
+
+#########################################################################
+# DONT EDIT BELOW THIS LINE!!! Broken Server is the reason !!!          #
+#########################################################################
+
+
+#	Find old logs and remove them
+find -type f -name 'crash_*.log' -mtime +${DAYS} -print | xargs /bin/rm -f;
+
+#	Start the UT-Server with all comands behind ucc-bin
+./ucc-bin $@
+
+#	Copy the Server.log to the Crash-Date.Log for better Fail-Reports and continuous restarts
+cp UT2004.log crash_$STAMP.log
